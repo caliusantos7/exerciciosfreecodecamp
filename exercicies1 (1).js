98d704cc6ec1978f698b75c7e24981a71ca2d3e4
Caliu <71888315+caliusantos7@users.noreply.github.com>
2023-01-27 20:32:45 -0300

    Add files via upload

---
+++ exercicies1 (1).js
@@ -0,0 +1,143 @@
+//  // The activities below were created by artificial intelligence, using a language model trained by neural learning.
+
+console.log("###################### 1 ######################")
+// Write a function called "greatestNumber" that takes two numbers as parameters and returns the larger number.
+function maiorNumero(numUm, numDois) {
+    let numUm = 1;
+    let numDois = 2;
+    
+    console.log(numDois);
+}
+
+
+
+console.log("###################### 2 ######################")
+// Write a program that takes an integer and then uses a loop to print all numbers from 1 to the entered number.
+function numeroInteiro() {
+    let numeroInicial = 10;
+
+    while ( contador > numeroInicial) {
+        contador = 1;
+        console.log(numeroInicial);
+    }
+}
+
+
+
+console.log("###################### 3 ######################")
+// Write a program that takes a word and then uses a loop to print each letter of the word on a different line.
+function palavra() {
+    let texto = "palavra";
+    let silaba = 10;
+    while (contador == silaba) {
+        let contador = 0;
+        console.log(texto);
+        contador++;
+    }
+}
+
+
+console.log("###################### 4 ######################")
+// Write a function called "multiplyTable" that takes a number as a parameter and prints the multiplication table for that number from 1 to 10.
+function multiplTable(principalNumber) {
+    let principalNumber = 7;
+    
+    while (contador < 11) {
+        var contador = 0;
+        
+        console.log(principalNumber)
+        contador++;
+    }
+    
+}
+
+
+
+console.log("###################### 5 ######################")
+// Write a program that takes an age as a parameter and then uses a conditional structure to check whether the person is of legal age (18 years or older) or a minor.
+function idade(idade) {
+    let  idade = 0 ;
+    
+    if (idade > 18) {
+        console.log("Parabéns voce é maior de idade");
+    } else {
+        console.log("Voce não é maior de idade");
+    }
+}
+
+
+
+console.log("###################### 6 ######################")
+// Write a function called "calculator" that takes three parameters: two numbers and a math operation (for example, "+", "-", "*", "/"). The function must return the result of the mathematical operation applied to the two numbers.
+function calculadora(primeiroNum, segundoNum, multiplicacao) {
+    let primeiroNum = 1;
+    let segundoNum = 2;
+    let multiplicacao = Number(5*5);
+    let total = multiplicacao + primeiroNum + segundoNum;
+
+    console.log(total);
+}
+
+
+
+console.log("###################### 7 ######################")
+// Write a program that uses a loop to read three numbers from the user and print the largest one.
+function leitura(params) {
+    let numberOne = 1;
+    let numberTwo =2;
+    let numberThree = 3;
+
+    console.log(numberOne);
+}
+
+
+
+console.log("###################### 8 ######################")
+// Write a function called "fibonacci" that takes an integer as a parameter and returns the nth number of the Fibonacci sequence.
+function finabocci(numeroInteiro) {
+    let numeroInteiro = 1;
+
+    let finaPrimeiro = Number(1 + 2) ; 
+    let finaSegundo = Number(2 + 3);
+    let finaTerceiro = (3 + 5);
+    let total = finaPrimeiro + finaSegundo + finaTerceiro;
+
+    console.log(numeroInteiro);
+    return total === numeroInteiro;
+}
+
+
+
+console.log("###################### 9 ######################")
+// Write a program that uses a loop to read a sequence of numbers passed as a parameter. The program must print the sum of the numbers read.
+function sequencia(passadoprimeiro, passadosegundo, passadoterceiro) {
+    let passadoprimeiro = 1;
+    let passadosegundo = 2;
+    let passadoterceiro = 3;
+    let total = passadoprimeiro + passadosegundo + passadoterceiro;
+
+    while (contador == total) {
+        var contador = 0 ;
+        
+        console.log(total);
+        contador++;
+    }
+}
+
+
+
+console.log("###################### 10 ######################")
+// Write a function called "checkPalindrome" that takes a string as a parameter and returns "true" if the string is a palindrome (that is, if the string is the same when read backwards) and "false" otherwise.
+function checkPalindrome(texto) {
+    let texto = "nome";
+    let palidromo = "ANA";
+    
+    if (texto == isNaN) {
+        console.log(texto)
+        return texto == palidromo
+    } else {
+        console.log()
+    }
+}
+
+
